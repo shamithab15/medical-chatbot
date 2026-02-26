19edd2ddcf3bc004b0b0ba7fc2321eb5e4fcbaa9
shamitha <botcha.shamitha@inverv.com>
2026-02-26 20:59:39 +0530

    Updated medical chatbot with Groq integration

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, jsonify, request
 from src.helper import download_hugging_face_embeddings
 from langchain_pinecone import PineconeVectorStore
-from langchain_openai import ChatOpenAI
+from groq import Groq
+from langchain_groq import ChatGroq
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
@@ -15,11 +16,12 @@ app = Flask(__name__)
 
 load_dotenv()
 
+
 PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
-OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
 
 
 embeddings = download_hugging_face_embeddings()
@@ -36,13 +38,30 @@ docsearch = PineconeVectorStore.from_existing_index(
 
 retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
 
-chatModel = ChatOpenAI(model="gpt-4o")
+# chatModel = ChatOpenAI(model="gpt-4o")
+# prompt = ChatPromptTemplate.from_messages(
+#     [
+#         ("system", system_prompt),
+#         ("human", "{input}"),
+#     ]
+# )
+
+# Use Groq instead of OpenAI
+chatModel = ChatGroq(
+    model="llama-3.1-8b-instant",
+    groq_api_key=GROQ_API_KEY,
+    temperature=0
+)
+
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
         ("human", "{input}"),
     ]
 )
+
+
+
 
 question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
 rag_chain = create_retrieval_chain(retriever, question_answer_chain)
